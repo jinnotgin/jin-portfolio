233a3ee892ec9fad92585d88d27fc6ec41def07b
Jin <me@thelinjin.com>
2020-02-29 13:11:49 +0800

    Updated bio

--- src/hero/Hero.js
+++ src/hero/Hero.js
@@ -10,7 +10,25 @@ class Hero extends Component {
       <section className="hero">
         <div className="container container--horizontally-spaced personal">
           <h1 className="hero__title header">
-            I’m Jin, a <a className="inverse" href="https://www.mindtheproduct.com/2011/10/what-exactly-is-a-product-manager/" target="_blank">product manager</a>, <a className="inverse" href="https://uxplanet.org/ux-engineers-what-we-are-2b0c4d10b3a6" target="_blank">UX engineer</a> and <a className="inverse" href="https://github.com/jinnotgin" target="_blank">developer</a> who leverages on technology to unlock efficiencies within everyday processes.
+            {/* I’m Jin, a <a className="inverse" href="https://www.mindtheproduct.com/2011/10/what-exactly-is-a-product-manager/" target="_blank">product manager</a>, <a className="inverse" href="https://uxplanet.org/ux-engineers-what-we-are-2b0c4d10b3a6" target="_blank">UX engineer</a> and <a className="inverse" href="https://github.com/jinnotgin" target="_blank">developer</a> who leverages on technology to unlock efficiencies within everyday processes. */}
+            I’m Jin, a{" "}
+            <a
+              className="inverse"
+              href="https://github.com/jinnotgin"
+              target="_blank"
+            >
+              full stack developer
+            </a>{" "}
+            and an{" "}
+            <a
+              className="inverse"
+              href="https://www.agileconnection.com/article/modern-role-agile-business-analyst"
+              target="_blank"
+            >
+              agile business analyst
+            </a>{" "}
+            who leverages on technology to unlock efficiencies in everyday
+            processes.
           </h1>
           <div className="hero__headshot">
             <img className="hero__headshot-image" src={profilepic_jin} />
